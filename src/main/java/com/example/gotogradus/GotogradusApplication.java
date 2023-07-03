78410cdd71d86b45393e744a8a78c8368bad78c6
itpark <a.m.smyslov@yandex.ru>
2023-07-03 21:17:46 +0300

    Initial commit

---
+++ src/main/java/com/example/gotogradus/GotogradusApplication.java
@@ -0,0 +1,13 @@
+package com.example.gotogradus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotogradusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GotogradusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gotogradus/GotogradusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gotogradus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotogradusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
