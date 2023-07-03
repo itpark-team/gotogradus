2f2988e9d0a94d7f15e82896435c5c4e5c1839fe
itpark <a.m.smyslov@yandex.ru>
2023-07-03 21:33:36 +0300

    add product entity

---
+++ src/main/java/com/example/gotogradus/dtos/Product.java
@@ -0,0 +1,47 @@
+package com.example.gotogradus.dtos;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int id;
+
+    @Column(name = "name")
+    public String name;
+
+    @Column(name = "volume")
+    public BigDecimal volume;
+
+    @Column(name = "price")
+    public BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    public Country country;
+
+    @Column(name = "description")
+    public String description;
+
+    @Column(name = "alcohol_percent")
+    public BigDecimal alcoholPercent;
+
+    @Column(name = "picture_path")
+    public String picturePath;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    public Category categoryId;
+
+    @Column(name = "is_active")
+    public boolean isActive;
+}
